dc0d19322e68219d31fc3ea47bc26554390dd32a
kitsu6922 <44791314+kitsu6922@users.noreply.github.com>
2021-12-14 01:18:46 -0700

    Unidad5
    Evaluacion
    Mariely Salais
    Mario Garcia
    
    Co-Authored-By: MarielySalais <71899228+MarielySalais@users.noreply.github.com>

--- compilador.py
+++ compilador.py
@@ -1,6 +1,41 @@
-
 from lex import *
 from tokentype import *
+import sintaxys
+from PyQt5 import QtWidgets
+from window import *
+
+class MainWindow(QtWidgets.QMainWindow,Ui_MainWindow):
+    def __init__(self,editor,*args,**kwargs):
+        QtWidgets.QMainWindow.__init__(self,*args,**kwargs)
+        self.setupUi(self,editor)
+        self.editor=editor
+        self.btnRun.clicked.connect(self.compilar)
+    def compilar(self):
+        lexer=Lexer(self.editor.toPlainText())
+        token = lexer.getToken()
+        cont = 0
+        todo=""
+        while token.kind != TokenType.EOF:
+            todo+="Token Type: {} , Content: {} \n".format(token.kind, token.text)
+            token = lexer.getToken()
+            cont+=1
+        todo+="\nNumber tokens found: {} ".format(cont)
+        self.txtConsola.setPlainText(todo)
+
+if __name__ == '__main__':
+    app=QtWidgets.QApplication([])
+    editor=QtWidgets.QPlainTextEdit()
+    editor.setStyleSheet("""QPlainTextEdit{
+        color:#ccc;
+        background-color:#2b2b2b;
+        font-family:'consolas';
+    }""")
+    file=open("./code.pvm","r")
+    editor.setPlainText(file.read())
+    pintar=sintaxys.PythonHighlighter(editor.document())
+    window=MainWindow(editor)
+    window.show()
+    app.exec_()
 
 def main():
     file = open("./code.pvm","r")
@@ -13,4 +48,4 @@ def main():
         token = lexer.getToken()
         cont+=1
     print("Number tokens found: {} ".format(cont))
-main()
+#main()
